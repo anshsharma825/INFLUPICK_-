cfc4380badddec090127e7f52ba53163a9024fd1
anshsharma825 <anshmanu2006@gmail.com>
2025-07-03 03:17:21 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true,
     domains: ['localhost'],
   },
-}
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
